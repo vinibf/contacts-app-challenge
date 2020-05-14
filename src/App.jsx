617bb993419e6db12d7ab2a65f009f78515aea13
Vinicius BF <vini91bf@gmail.com>
2020-05-13 23:19:06 -0300

    Filters component modularized

--- src/App.jsx
+++ src/App.jsx
@@ -13,37 +13,7 @@ class App extends React.Component {
       <React.Fragment>
         <Topbar />
 
-        <div className="container">
-          <section className="filters">
-            <div className="filters__search">
-              <input type="text" className="filters__search__input" placeholder="Pesquisar" />
-
-              <button className="filters__search__icon">
-                <i className="fa fa-search"/>
-              </button>
-            </div>
-
-            <button className="filters__item is-selected">
-              Nome <i className="fas fa-sort-down" />
-            </button>
-
-            <button className="filters__item">
-              País <i className="fas fa-sort-down" />
-            </button>
-
-            <button className="filters__item">
-              Empresa <i className="fas fa-sort-down" />
-            </button>
-
-            <button className="filters__item">
-              Departamento <i className="fas fa-sort-down" />
-            </button>
-
-            <button className="filters__item">
-              Data de admissão <i className="fas fa-sort-down" />
-            </button>
-          </section>
-        </div>
+        <Filters />
 
         <div className="container">
           <section className="contacts">
